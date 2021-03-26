de016497abd0d96dc2928f347931b04e2b0ad000
Christopher Chantres <chris.chantres@gmail.com>
2021-03-25 20:19:17 -0600

    Ya está casi listo para hacer el deploy, se agregaron las advertencias en la consola del navegador

---
+++ javascript/script.js
@@ -0,0 +1,2 @@
+console.log("%cAdvertencia","color:white;font-size: 16px;background:#FF0000;font-weight: bold;");
+console.log("%cHola, este es un espacio para desarrolladores, no intentes de hacer algún tipo de hack, pues no funcionará, solamente cierra esta ventana presinando la X de la parte superior derecha.","color:white;font-size: 14px;background:#335794;font-weight: bold;");
